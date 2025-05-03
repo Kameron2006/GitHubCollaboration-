bfba4d9d850a943752fa287aacf466e091b6710e
Kameron H <kam.harvey10@gmail.com>
2025-05-03 16:23:42 -0400

    Initial Commit

---
+++ GitHubCollaboration!/GitHubCollaboration_App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaboration_App.swift
+//  GitHubCollaboration!
+//
+//  Created by Scholar on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaboration_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubCollaboration!Tests/GitHubCollaboration_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaboration_Tests.swift
+//  GitHubCollaboration!Tests
+//
+//  Created by Scholar on 5/3/25.
+//
+
+import Testing
+@testable import GitHubCollaboration_
+
+struct GitHubCollaboration_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
