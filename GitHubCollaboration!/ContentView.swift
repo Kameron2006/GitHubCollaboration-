27d2ba42f189cb82fd762ef120c5f442ff27ac52
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2025-05-03 16:18:51 -0500

    Add change 4

--- GitHubCollaboration!/ContentView.swift
+++ GitHubCollaboration!/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("Change 1!")
             Text("Change 2")
             Text("Change 3 - Kait")
+            Text("Change 4")
         }
         .padding()
     }
